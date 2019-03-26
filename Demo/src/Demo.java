90f01610d4714da75b56b0db13abbca7fd0604ef
陈芳杰 <fangjiechen@91jkys.com>
2019-03-26 15:52:35 +0800

    Demo
    Demo

---
+++ Demo/src/Demo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+class Test {
+	String s = "aaa";
+}
+
+public class Demo {
+	public static void main(String[] args) {
+		Test t = new Test();
+		print(t.s);
+	}
+}
