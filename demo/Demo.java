57ac7e7b995aba282ed615b2ddfbeafc08587e51
陈芳杰 <fangjiechen@91jkys.com>
2019-09-25 10:53:25 +0800

    Demo
    Demo

--- demo/Demo.java
+++ demo/Demo.java
@@ -1,30 +1,22 @@
+import java.util.*;
+import java.util.regex.*;
+
 public class Demo {
-	static <T> void f1(Holder<T> holder) {
-		T t = holder.get();
-		System.out.println(t.getClass().getSimpleName());
-	}
-
-	static void f2(Holder<?> holder) {
-		f1(holder);    // 捕获转换
-	}
-
-    //@SuppressWarnings("unchecked")
 	public static void main (String[] args) {
-		Holder raw = new Holder<Integer>(1);
-		//f1(raw);
-		f2(raw);
-
-		Holder rawBasic = new Holder();
-		rawBasic.set(new Object());    // warning
-		f2(rawBasic);                  // no warning
-
-		Holder<?> wildcarded = new Holder<Double>(1.0);
-		f2(wildcarded);
+		String str="123456我是JAVA{, ~!@asd";
+ 		String rex="^";
+ 
+ 		Pattern pattern=Pattern.compile(rex);
+ 		Matcher matcher=pattern.matcher(str);
+ 
+ 		String [] result=pattern.split(str); 
+ 
+ 		for(String string:result) {
+ 			System.out.println("分割的字符串:"+"["+string+"]");
+ 		}
 	}
 }
 
 /*
-Integer
-Object
-Double
+
 */
