1531fad40f2d4551697e90c37a82e3dfc857cd48
c007136 <c007136@163.com>
2019-10-01 08:40:37 +0800

    feat(java): java

--- demo/Demo.java
+++ demo/Demo.java
@@ -1,22 +1,59 @@
 import java.util.*;
-import java.util.regex.*;
+
+enum Input {
+  NICKEL(5), 
+  DIME(10), 
+  QUARTER(25), 
+  DOLLAR(100),
+  TOOTHPASTE(200), 
+  CHIPS(75), 
+  SODA(100), 
+  SOAP(50),
+  ABORT_TRANSACTION {
+    public int amount() { // Disallow
+      throw new RuntimeException("ABORT.amount()");
+    }
+  },
+  STOP { // This must be the last instance.
+    public int amount() { // Disallow
+      throw new RuntimeException("SHUT_DOWN.amount()");
+    }
+  };
+  
+  int value; // In cents
+  Input(int value) { this.value = value; }
+  Input() {}
+  int amount() { return value; }; // In cents
+  static Random rand = new Random(47);
+  public static Input randomSelection() {
+    // Don't include STOP:
+    return values()[rand.nextInt(values().length - 1)];
+  }
+}
+
 
 public class Demo {
-	public static void main (String[] args) {
-		String str="123456我是JAVA{, ~!@asd";
- 		String rex="^";
- 
- 		Pattern pattern=Pattern.compile(rex);
- 		Matcher matcher=pattern.matcher(str);
- 
- 		String [] result=pattern.split(str); 
- 
- 		for(String string:result) {
- 			System.out.println("分割的字符串:"+"["+string+"]");
- 		}
+	public static void main(String[] args) {
+		for (Mail mail : Mail.generator(2)) {
+			System.out.println(mail.details());
+			PostOffice.handle(mail);
+			System.out.println("-----------");
+		}
 	}
 }
 
-/*
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
