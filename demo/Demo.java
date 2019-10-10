a250e03019c8155ef82200bf610135412e7dd50f
c007136 <c007136@163.com>
2019-10-11 07:46:57 +0800

    feat(java): java

--- demo/Demo.java
+++ demo/Demo.java
@@ -1,45 +1,83 @@
 import java.util.*;
+import java.io.*;
 
-enum Input {
-  NICKEL(5), 
-  DIME(10), 
-  QUARTER(25), 
-  DOLLAR(100),
-  TOOTHPASTE(200), 
-  CHIPS(75), 
-  SODA(100), 
-  SOAP(50),
-  ABORT_TRANSACTION {
-    public int amount() { // Disallow
-      throw new RuntimeException("ABORT.amount()");
-    }
-  },
-  STOP { // This must be the last instance.
-    public int amount() { // Disallow
-      throw new RuntimeException("SHUT_DOWN.amount()");
-    }
-  };
-
-  int value; // In cents
-  Input(int value) { this.value = value; }
-  Input() {}
-  int amount() { return value; }; // In cents
-  static Random rand = new Random(47);
-  public static Input randomSelection() {
-    // Don't include STOP:
-    return values()[rand.nextInt(values().length - 1)];
-  }
+enum Outcome {
+	WIN,
+	LOSE,
+	DRAW
 }
 
+interface Competitor<T extends Competitor<T>> {
+	Outcome compete(T Competitor);
+}
 
-public class Demo {
+class Enums {
+	private static Random rand = new Random(47);
+
+	public static <T extends Enum<T>> T random(Class<T> ec) {
+		return random(ec.getEnumConstants());
+	}
+
+	public static <T> T random(T[] values) {
+		return values[rand.nextInt(values.length)];
+	}
+}
+
+class RoShamBo {
+	public static <T extends Competitor<T>> void match(T a, T b) {
+		System.out.println(a + " vs. " + b + ": " + a.compete(b));
+	}
+
+	public static <T extends Enum<T> & Competitor<T>> void play(Class<T> rsbclass, int size) {
+		for (int i = 0; i < size; i++) {
+			match(Enums.random(rsbclass), Enums.random(rsbclass));
+		}
+	}
+}
+
+public enum Demo implements Competitor<Demo> {
+	PAPER(Outcome.DRAW, Outcome.LOSE, Outcome.WIN),
+	SCISSORS(Outcome.WIN, Outcome.DRAW, Outcome.LOSE),
+	ROCK(Outcome.LOSE, Outcome.WIN, Outcome.DRAW);
+
+	private Outcome vPaper;
+	private Outcome vScissors;
+	private Outcome vRock;
+
+	Demo(Outcome paper, Outcome scissors, Outcome rock) {
+		vPaper = paper;
+		vScissors = scissors;
+		vRock = rock;
+	}
+
+	public Outcome compete(Demo it) {
+		switch (it) {
+			default:
+			case PAPER: return vPaper;
+			case SCISSORS: return vScissors;
+			case ROCK: return vRock;
+		}
+	}
+
+
 	public static void main(String[] args) {
-		
+		RoShamBo.play(Demo.class, 10);
 	}
 }
 
 
-
+/*
+ROCK vs. ROCK: DRAW
+SCISSORS vs. ROCK: LOSE
+SCISSORS vs. ROCK: LOSE
+SCISSORS vs. ROCK: LOSE
+PAPER vs. SCISSORS: LOSE
+PAPER vs. PAPER: DRAW
+PAPER vs. SCISSORS: LOSE
+ROCK vs. SCISSORS: WIN
+SCISSORS vs. SCISSORS: DRAW
+ROCK vs. SCISSORS: WIN
+*/
 
 
 
